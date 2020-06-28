2c5af3a7dbde6e9c3ba437aecb3ebf5b67bf9d5e
Gunalan S <34304620+s-gunalan@users.noreply.github.com>
2020-06-28 08:33:33 +0530

    Add files via upload

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def home(request):
-    return render(request,'base.html')
+    pro=Product.objects.all()
+    context = {
+        'proj_list':pro,
+    }
+    return render(request,'base.html',context)
